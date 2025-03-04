0bb08f9b344fc5882f2f699bdf52e49ae493e82d
JKGLP <fjavierlopezpuertas@gmail.com>
2025-03-04 16:11:56 +0100

    Añadido CV y pequeños cambios

--- js/main.js
+++ js/main.js
@@ -191,19 +191,17 @@
   	/* Smooth Scrolling
   	------------------------------------------------------ */
   	$('.smoothscroll').on('click', function (e) {
-	 	
-	 	e.preventDefault();
+    e.preventDefault();
 
-   	var target = this.hash,
-    	$target = $(target);
+    var target = this.hash,
+        $target = $(target);
 
-    	$('html, body').stop().animate({
-       	'scrollTop': $target.offset().top
-      }, 800, 'swing', function () {
-      	window.location.hash = target;
-      });
-
-  	});  
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 800, 'swing', function () {
+        window.location.hash = target;
+    });
+}); 
   
 
    /*---------------------------------------------------- */
